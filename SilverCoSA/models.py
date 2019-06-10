d11a24f7c2efa4af78f0ce98a3fb730f3151d716
JavCarFer <javcarfer92@gmail.com>
2019-06-10 14:29:30 +0200

    Creating models

--- SilverCoSA/models.py
+++ SilverCoSA/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(default="")
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=40,default="")
+    place = Place
+    isKing= models.BooleanField()
+    isDead=models.BooleanField(initial=True)
+    def __str__(self):
+        return self.nombre
+
+class Stat(models.Model):
+    name = models.CharField(max_length=40, default="")
+    description = models.CharField(max_length=200,default="")
+    def __str__(self):
+        return self.name
+
+class Rule(models.Model):
+    name= models.CharField(max_length=40,default="Capa")
+    description = models.CharField(max_length=200,default="")
+    def __str__(self):
+        return self.name
